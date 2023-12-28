309097a28f1295b8b5d487a2f61c246b3f735c23
Dani Salanova <dasadmx@gmail.com>
2023-12-28 17:45:10 +0100

    cambios

---
+++ Dartam/src/analizadorSintactico/symbols/SymbolAdd.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package analizadorSintactico.symbols;
+
+/**
+ *
+ * @author dasad
+ */
+public class SymbolAdd {
+    public double result;
+    
+    public SymbolAdd(double v1, double v2){
+        result = v1 + v2;
+    }
+    
+}
