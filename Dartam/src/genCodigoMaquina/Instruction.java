515071afe218d3c4701b18fc37de9b4a9f03a227
Arturo <69714460+Jarturog@users.noreply.github.com>
2024-01-21 18:32:55 +0100

    hashtable

--- Dartam/src/genCodigoMaquina/Instruction.java
+++ Dartam/src/genCodigoMaquina/Instruction.java
@@ -35,65 +35,36 @@ public class Instruction {
 
     @Override
     public String toString(){
-        switch (instruction) {
-            case copy:
-                return "\t" + destination + " = " + left;
-            case add:
-                return "\t" + destination + " = " + left + " + " + right;
-            case sub:
-                return "\t" + destination + " = " + left + " - " + right;
-            case prod:
-                return "\t" + destination + " = " + left + " * " + right;
-            case and:
-                return "\t" + destination + " = " + left + " and " + right;
-            case call:
-                return "\t" + destination + " = call " + left;
-            case div:
-                return "\t" + destination + " = " + left + " / " + right;
-            case go_to:
-                return "\tgoto " + destination;
-            case if_EQ:
-                return "\tif " + left + "=" + right + " goto " + destination;
-            case if_GE:
-                return "\tif " + left + ">=" + right + " goto " + destination;
-            case if_GT:
-                return "\tif " + left + ">" + right + " goto " + destination;
-            case if_LE:
-                return "\tif " + left + "<=" + right + " goto " + destination;
-            case if_LT:
-                return "\tif " + left + "<" + right + " goto " + destination;
-            case if_NE:
-                return "\tif " + left + "!=" + right + " goto " + destination;
-            case ind_ass:
-                return "\t" + destination + "[" + left + "] = " + right;
-            case ind_val:
-                return "\t" + destination + " = " + left + "[" + right + "]";
-            case mod:
-                return "\t" + destination + " = " + left + " mod " + right;
-            case neg:
-                return "\t" + destination + " = -" + left; 
-            case not:
-                return "\t" + destination + " = not " + left;
-            case or:
-                return "\t" + destination + " = " + left + " or " + right;
-            case param_c:
-                return "\tparam_c: " + destination + "[]";// + left + "]";
-            case param_s:
-                return "\tparam_s: " + destination;
-            case pmb:
-                return "\tpmb " + destination;
-            case rtn:
-                return "\trtn " + destination + ": " + left;
-            case skip:
-                return destination + ": skip";
-            case in:
-                return "\tin: " + destination;
-            case out:
-                return "\tout: " + destination;
-            case point:
-                return "\t" + destination + " => " + left;
-            default:
-                return null;
-        }
+        return switch (instruction) {
+            case copy -> "\t" + destination + " = " + left;
+            case add -> "\t" + destination + " = " + left + " + " + right;
+            case sub -> "\t" + destination + " = " + left + " - " + right;
+            case prod -> "\t" + destination + " = " + left + " * " + right;
+            case and -> "\t" + destination + " = " + left + " and " + right;
+            case call -> "\t" + destination + " = call " + left;
+            case div -> "\t" + destination + " = " + left + " / " + right;
+            case go_to -> "\tgoto " + destination;
+            case if_EQ -> "\tif " + left + "=" + right + " goto " + destination;
+            case if_GE -> "\tif " + left + ">=" + right + " goto " + destination;
+            case if_GT -> "\tif " + left + ">" + right + " goto " + destination;
+            case if_LE -> "\tif " + left + "<=" + right + " goto " + destination;
+            case if_LT -> "\tif " + left + "<" + right + " goto " + destination;
+            case if_NE -> "\tif " + left + "!=" + right + " goto " + destination;
+            case ind_ass -> "\t" + destination + "[" + left + "] = " + right;
+            case ind_val -> "\t" + destination + " = " + left + "[" + right + "]";
+            case mod -> "\t" + destination + " = " + left + " mod " + right;
+            case neg -> "\t" + destination + " = -" + left;
+            case not -> "\t" + destination + " = not " + left;
+            case or -> "\t" + destination + " = " + left + " or " + right;
+            case param_c -> "\tparam_c: " + destination + "[]";
+            case param_s -> "\tparam_s: " + destination;
+            case pmb -> "\tpmb " + destination;
+            case rtn -> "\trtn " + destination + ": " + left;
+            case skip -> destination + ": skip";
+            case in -> "\tin: " + destination;
+            case out -> "\tout: " + destination;
+            case point -> "\t" + destination + " => " + left;
+            default -> null;
+        }; // + left + "]";
     }
 }
