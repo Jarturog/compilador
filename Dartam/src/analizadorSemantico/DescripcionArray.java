1eabb4b4d3e0dc17b15f2347d3af160e48bd7434
Arturo <69714460+Jarturog@users.noreply.github.com>
2024-01-26 23:15:15 +0100

    varias Descripcion

--- Dartam/src/analizadorSemantico/DescripcionArray.java
+++ Dartam/src/analizadorSemantico/DescripcionArray.java
@@ -2,20 +2,21 @@ package analizadorSemantico;
 
 import analizadorSintactico.symbols.SymbolOperand;
 import java.util.ArrayList;
+import jflex.base.Pair;
 
 public class DescripcionArray extends DescripcionSimbolo {
+
     private ArrayList<SymbolOperand> dimensiones;
     public int dcamp; //Desplazamiento dentro del array
+
     /**
      * Array
      */
-    public DescripcionSimbolo(String t, ArrayList<SymbolOperand> dim, DescripcionSimbolo tupla) {
-        tipo = t;
+    public DescripcionArray(String t, ArrayList<SymbolOperand> dim, DescripcionSimbolo tupla, DescripcionDefinicionTupla tipoTupla) {
+        super(t, false, false, tupla, tipoTupla);
         dimensiones = dim;
-        memberOf = tupla; // miembro de una tupla
-        miembros = null;
     }
-    
+
     //    //Array
 //    public void setTipoBase(SymbolTipo base) {
 //        //tipo.setTipoBase(base);
@@ -32,4 +33,15 @@ public class DescripcionArray extends DescripcionSimbolo {
 //    public void setTamanyoArray(int n) {
 //        //tipo.setTamanyoArray(n);
 //    }
+    @Override
+    public String toString() {
+
+        String dim = "";
+        for (SymbolOperand op : dimensiones) {
+            dim += op + " ";
+        }
+        String s = "Array de tipo '" + tipo + "' con dimensiones " + dim.substring(0, dim.length() - 1);
+
+        return s + " declarado en el nivel " + nivel;
+    }
 }
